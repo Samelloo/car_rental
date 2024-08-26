d68abaa5eeff59dc27971afeb26fd432df863750
Developer TBDigital <developertbdigital@Developers-Laptop.local>
2024-08-26 15:06:15 +0800

    fixed : error on coming soon page due the function error

--- app/(main)/comingSoon/page.tsx
+++ app/(main)/comingSoon/page.tsx
@@ -7,13 +7,13 @@ const HomePageError = ({ err }: { err: Error; reset: () => void }) => {
   return (
     <div className="w-screen h-screen flex flex-col gap-y-4 justify-center items-center fixed top-0 left-0 z-[10000] bg-white/55 backdrop-blur-sm">
       <h1 className="text-2xl font-thin text-dark-400 text-center">
-        {err?.message || "This Features Still under Constructions !"}
+        {err?.message || "Something went wrong !"}
       </h1>
       <div className="flex gap-x-4 items-center">
         <Link href="/advance">
           {" "}
           <Button className="w-28" variant="secondary">
-            Back
+            Try again
           </Button>
         </Link>
       </div>
